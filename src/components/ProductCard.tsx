4e579aca94b0a525880927e9e0ec881d4da2821c
Swapnodeep Biswas <bonny2610@gmail.com>
2024-01-16 02:10:23 +0530

    Complete Home UI & Product Card

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,36 @@
+import { FaPlus } from "react-icons/fa";
+
+type ProductsProps = {
+  productId: string;
+  photo: string;
+  name: string;
+  price: number;
+  stock: number;
+  handler: () => void;
+};
+
+const server = "asfajfkdlaj";
+
+export default function ProductCard({
+  productId,
+  photo,
+  name,
+  price,
+  stock,
+  handler,
+}: ProductsProps) {
+  return (
+    <div className="product-card">
+      <img src={photo} alt={name} />
+      <p>{name}</p>
+      <span>₹{price}</span>
+
+      {/* Overlay button */}
+      <div>
+        <button onClick={() => handler()}>
+          <FaPlus />
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,31 @@
+import { Link } from "react-router-dom";
+import ProductCard from "../components/ProductCard";
 
 export default function Home() {
-  return <div>Home</div>;
+  const addToCartHandler = () => {
+    //
+  };
+  return (
+    <div className="home">
+      <section></section>
+
+      <h1>
+        Latest Products
+        <Link to="/search" className="findMore">
+          More
+        </Link>
+      </h1>
+
+      <main>
+        <ProductCard
+          productId="sdjfhaldksjfha"
+          name="Apple MacBook Air 2024"
+          price={81990}
+          stock={435}
+          handler={addToCartHandler}
+          photo="https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1685966374/Croma%20Assets/Computers%20Peripherals/Laptop/Images/256711_umnwok.png?tr=w-360"
+        />
+      </main>
+    </div>
+  );
 }
