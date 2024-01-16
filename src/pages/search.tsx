877d473c691d8a5888733b5f42733d2c31879d84
Swapnodeep Biswas <bonny2610@gmail.com>
2024-01-16 19:08:18 +0530

    Complete Search Page UI

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,4 +1,94 @@
+import { useState } from "react";
+import ProductCard from "../components/ProductCard";
 
 export default function Search() {
-  return <div>Search</div>;
+  const [search, setSearch] = useState<string>("");
+  const [sort, setSort] = useState<string>("");
+  const [maxPrice, setMaxPrice] = useState<number>(100000);
+  const [category, setCategory] = useState<string>("");
+  const [page, setPage] = useState<number>(1);
+
+  const addToCartHandler = () => {};
+
+  const isPrevPage = page > 1;
+  const isNextPage = page < 4;
+
+  return (
+    <div className="product-search-page">
+      <aside>
+        <h2>Filters</h2>
+
+        <div>
+          <h4>Sort</h4>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option>None</option>
+            <option value="asc">Price (Low to High)</option>
+            <option value="dsc">Price (High to Low)</option>
+          </select>
+        </div>
+
+        <div>
+          <h4>Max Price: {maxPrice || ""}</h4>
+          <input
+            type="range"
+            min={100}
+            max={100000}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(+e.target.value)}
+          />
+        </div>
+
+        <div>
+          <h4>Category</h4>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option>All</option>
+            <option value="">Sample1</option>
+            <option value="">Sample2</option>
+          </select>
+        </div>
+      </aside>
+
+      <main>
+        <h1>Products</h1>
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <div className="search-product-list">
+          <ProductCard
+            productId="sdjfhaldksjfha"
+            name="Apple MacBook Air 2024"
+            price={81990}
+            stock={435}
+            handler={addToCartHandler}
+            photo="https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1685966374/Croma%20Assets/Computers%20Peripherals/Laptop/Images/256711_umnwok.png?tr=w-360"
+          />
+        </div>
+
+        <article>
+          <button
+            disabled={!isPrevPage}
+            onClick={() => setPage((prev) => prev - 1)}
+          >
+            Prev
+          </button>
+          <span>
+            {page} of {4}
+          </span>
+          <button
+            disabled={!isNextPage}
+            onClick={() => setPage((next) => next + 1)}
+          >
+            Next
+          </button>
+        </article>
+      </main>
+    </div>
+  );
 }
